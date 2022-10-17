9e69030f2adce62557a340c5203f1323073f8582
Peter luque <Peterluque21@gmail.com>
2022-10-16 20:01:54 -0400

    commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace LINQ
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LINQ
+
 {
     internal class Program
+
     {
-        static void Main(string[] args)
+        static void main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            List<string> VideoGameNames = new List<string>
+        {"Madden",  "2k23", "Warzone" , "Modren Warefare" , "Midnight Club" };
+
+
+
+
+            // Method Syntax
+            Console.WriteLine("------Method Syntax------");
+            List<string> Games = new List<string>
+            {
+                "call of duty warzone",//4
+                "rocket leage",//3
+                "elden ring",//2
+                "ark" //1
+            };
+            //--------------------------------\
+
+            var Order = Games.OrderBy(x => x.Length);// took me so long to see i needed .length
+            foreach (string Game in Order)
+            {
+                Console.WriteLine(Game);
+            }
+            Console.WriteLine("-------------------------");
+            //Query Syntax
+            Console.WriteLine("------Query Syntax-------");
+            List<string> Gamestwo = new List<string>
+            {
+                "call of duty warzone",//4
+                "rocket leage",//3
+                "elden ring",//2
+                "ark" //1
+            };
+            var Length = from GameX in Gamestwo
+                         orderby GameX.Length ascending
+                         select GameX;
+            foreach (string GameX in Length)
+            {
+                Console.WriteLine(GameX);
+            }
+            Console.WriteLine("---------------------------");
         }
+
     }
 }
+
+
+
+
